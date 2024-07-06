706078bed92e10499159c9943ecac9432e6dfb75
TheMoonisBlueeee <thato4444@gmail.com>
2024-07-06 23:12:39 +0200

    Update match_case_calcualtor.py

--- control-flow/match_case_calcualtor.py
+++ control-flow/match_case_calcualtor.py
@@ -1,1 +1,17 @@
+number1=input("Please input the first number:")
+number2=input("Please input the second number:")
+operation=input("Please pick an order of operation (+,-,*,/):")
+result=number1
+
+match operation:
+    case "+":
+        print=float("The result is",result)
+    
+    case "-":
+           print=float("The result is",result)
+    
+    case "*":
+          print("The result is",reslut)
+
+    case "/":("The result is",result)     
 
