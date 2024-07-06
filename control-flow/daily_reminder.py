929cf9a8a32bce96a403dfa74a8314741f01c7ee
TheMoonisBlueeee <thato4444@gmail.com>
2024-07-06 23:37:28 +0200

    Update daily_reminder.py

--- control-flow/daily_reminder.py
+++ control-flow/daily_reminder.py
@@ -1,3 +1,3 @@
 task=input("Enter your task:")
-priority=input("Priority(high/medium/low:)")
-time=input("Is it time-bound (yes/no)")
+priority=input("Priority (high/medium/low:)")
+time=input("Is it time-bound (yes/no):")
