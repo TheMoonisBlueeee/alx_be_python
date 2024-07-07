853006b379509566e799026bccce740c477e13c1
TheMoonisBlueeee <thato4444@gmail.com>
2024-07-07 20:13:29 +0200

    Update multiplication_table.py

--- control-flow/multiplication_table.py
+++ control-flow/multiplication_table.py
@@ -1,5 +1,5 @@
-pattern=int(input("Enter the size of the pattern:"))
+number=int(input("Enter the size of the pattern:"))
 
 for i in range(1,11):
-    product= pattern * i
-    print(f"{pattern} * {i} = {product}")
+    product= number * i
+    print(f"{number} * {i} = {product}")
